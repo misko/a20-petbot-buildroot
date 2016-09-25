666c229ff05d90e375f579b93dc7230cf90a993e
misko <misko@petbot.com>
2016-09-26 02:43:24 +0800

    add tecate4 to setup wifis

--- target/linux/extras/test_wifi_sc.sh
+++ target/linux/extras/test_wifi_sc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-test_wifi /a20-petbot-firmware/ /a20-petbot-firmware/config/ /tmp/ '10F Lyton Building' 96854968
+#test_wifi /a20-petbot-firmware/ /a20-petbot-firmware/config/ /tmp/ '10F Lyton Building' 96854968
+test_wifi /a20-petbot-firmware/ /a20-petbot-firmware/config/ /tmp/ TECATE4 bobsburgers
