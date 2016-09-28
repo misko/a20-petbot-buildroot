dd2fab345dea77835ec695b2c7b623d707d62a30
misko <misko@petbot.com>
2016-09-29 07:14:27 +0800

    added in ORC support for gstreamer video format conversion

---
+++ package/orc/orc.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# orc
+#
+#############################################################
+ORC_VERSION = 0.4.25
+ORC_SITE = http://code.entropywave.com/download/orc/
+ORC_INSTALL_STAGING = YES
+ORC_DEPENDENCIES = host-orc
+
+define ORC_REMOVE_BUGREPORT
+	rm -f $(TARGET_DIR)/usr/bin/orc-bugreport
+endef
+
+define ORC_REMOVE_DEVFILES
+	rm -f $(TARGET_DIR)/usr/bin/orcc
+endef
+
+ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_BUGREPORT
+
+ifneq ($(BR2_HAVE_DEVFILES),y)
+ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_DEVFILES
+endif
+
+$(eval $(call AUTOTARGETS,package,orc))
+$(eval $(call AUTOTARGETS,package,orc,host))
+#$(eval $(autotools-package))
+#$(eval $(host-autotools-package))
