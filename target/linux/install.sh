9f209929ab492b234bd1f1d9975fba2d615cf62f
misko <misko@petbot.com>
2016-11-19 02:01:11 +0800

    dont include history networks in the final release

--- target/linux/install.sh
+++ target/linux/install.sh
@@ -5,6 +5,7 @@ chmod 700 ${root}/root/.ssh
 rm -rf ${root}/usr/share/gtk-doc
 rm -f ${root}/bin/CedarXPlayerTest
 rm -f ${root}/root/test1.mp4
+rm -rf ${root}/a20-petbot-firmware/networks/history
 #rm -f ${root}/a20-petbot-firmware/update_key #copied in part of mkcmd.sh
 #set it up so things boot
 #grep '::sysinit:/a20-petbot-firmware/scripts/load_all.sh' ${root}/etc/inittab
