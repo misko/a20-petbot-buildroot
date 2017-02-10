1860fa668d1c0a79262b32edbaa7ce7e6364f1e9
misko <misko@petbot.com>
2017-02-10 08:34:22 +0800

    upgrade libsoup

--- package/libsoup/libsoup.mk
+++ package/libsoup/libsoup.mk
@@ -1,29 +1,34 @@
-#############################################################
+################################################################################
 #
 # libsoup
 #
-#############################################################
+################################################################################
 
-LIBSOUP_MAJOR_VERSION:=2.31
-LIBSOUP_VERSION:=$(LIBSOUP_MAJOR_VERSION).2
-LIBSOUP_SOURCE:=libsoup-$(LIBSOUP_VERSION).tar.bz2
-LIBSOUP_SITE:=http://ftp.gnome.org/pub/gnome/sources/libsoup/$(LIBSOUP_MAJOR_VERSION)
+LIBSOUP_VERSION_MAJOR = 2.54
+LIBSOUP_VERSION = $(LIBSOUP_VERSION_MAJOR).1
+LIBSOUP_SOURCE = libsoup-$(LIBSOUP_VERSION).tar.gz
+LIBSOUP_SITE = http://ftp.gnome.org/pub/gnome/sources/libsoup/$(LIBSOUP_VERSION_MAJOR)
+LIBSOUP_LICENSE = LGPLv2+
+LIBSOUP_LICENSE_FILES = COPYING
 LIBSOUP_INSTALL_STAGING = YES
-
 LIBSOUP_CONF_ENV = ac_cv_path_GLIB_GENMARSHAL=$(LIBGLIB2_HOST_BINARY)
+LIBSOUP_CONF_OPT = --disable-glibtest --enable-vala=no --with-gssapi=no
+LIBSOUP_DEPENDENCIES = host-pkg-config host-libglib2 \
+	libglib2 libxml2 sqlite host-intltool
 
-ifneq ($(BR2_INET_IPV6),y)
-LIBSOUP_CONF_ENV += soup_cv_ipv6=no
+ifeq ($(BR2_PACKAGE_LIBSOUP_GNOME),y)
+LIBSOUP_CONF_OPT += --with-gnome
+else
+LIBSOUP_CONF_OPT += --without-gnome
 endif
 
-LIBSOUP_CONF_OPT = \
-	--enable-shared		\
-	--enable-static		\
-	--disable-explicit-deps \
-	--disable-glibtest	\
-	--disable-ssl		\
-	--without-gnome
+ifeq ($(BR2_PACKAGE_LIBSOUP_SSL),y)
+LIBSOUP_DEPENDENCIES += glib-networking
+else
+LIBSOUP_CONF_OPT += --disable-tls-check
+endif
 
-LIBSOUP_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl) host-pkg-config host-libglib2 libglib2 libxml2
+LIBSOUP_MAKE_OPT += CFLAGS+="-L../libsoup/.libs/" LIBS+="-lintl -lgio-2.0 -lgobject-2.0 -lglib-2.0"
 
+#$(eval $(autotools-package))
 $(eval $(call AUTOTARGETS,package,libsoup))
